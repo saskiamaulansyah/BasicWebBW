23d3042de51b3196a141e3267e6ff5f2432b3779
saskiamaulansyah <saskiamaulansyah@gmail.com>
2016-11-18 20:26:15 +0700

    Kuis HTML Kelima

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/Kuis5Activity.java
@@ -0,0 +1,53 @@
+package id.sch.smktelkom_mlg.project.xirpl403122130.basicweb;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Kuis5Activity extends Activity implements View.OnClickListener {
+    ImageView alert, alert2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuis5);
+        alert = (ImageView) findViewById(R.id.imageView);
+        alert.setOnClickListener(this);
+        alert2 = (ImageView) findViewById(R.id.imageView2);
+        alert2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == alert) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Jawaban Anda : ")
+                    .setMessage("SALAH")
+                    .setNeutralButton("Tutup", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dlg, int sumthin) {
+
+                        }
+                    })
+                    .show();
+        } else if (view == alert2) {
+
+            new AlertDialog.Builder(this)
+                    .setTitle("Jawaban Anda : ")
+                    .setMessage("BENAR")
+                    .setNeutralButton("Tutup", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dlg, int sumthin) {
+
+                        }
+                    })
+                    .show();
+        }
+    }
+}
