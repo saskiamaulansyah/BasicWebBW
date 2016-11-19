510528f93388e7c5a0c7b9d030a37db2f733107e
alyssavelia <alyssavelia2980@gmail.com>
2016-11-19 17:27:13 +0700

    initial commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/MainActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xirpl403122130.basicweb;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
