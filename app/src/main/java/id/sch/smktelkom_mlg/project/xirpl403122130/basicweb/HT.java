62b8a35351b57a4fc671d98b2c10011fd08f0b1f
alyssavelia <alyssavelia2980@gmail.com>
2016-11-19 20:59:10 +0700

    membuat content

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/HT.java
@@ -0,0 +1,15 @@
+package id.sch.smktelkom_mlg.project.xirpl403122130.basicweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ht);
+
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xirpl403122130/basicweb/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xirpl403122130.basicweb;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.imageView1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HT.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.imageView111).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, KuisActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
